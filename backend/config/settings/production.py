2551e8839b097fc0a839ae7a431419497685ee31
tenzin <ten13zin@gmail.com>
2021-06-12 13:37:09 +0530

    add: setting for local and prod

---
+++ backend/config/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env("DJANGO_SECRET_KEY")
