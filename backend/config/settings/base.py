52463b85f57a6447d9d1a315f31d49f9ee14f708
tenzin <ten13zin@gmail.com>
2021-06-13 12:11:56 +0530

    add: drf

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -73,6 +73,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-party
+    "rest_framework",
     # local
     "dakje.users.apps.UsersConfig",
 ]
--- backend/dakje/users/models.py
+++ backend/dakje/users/models.py
@@ -2,4 +2,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    """Default user for dakje."""
+
     pass
