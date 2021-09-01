4015abb6f35816ee19286e0de6d5a2b0ae6f708d
tenzin <ten13zin@gmail.com>
2021-09-01 22:53:20 +0530

    add: i18n

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -23,6 +23,7 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       'axios',
+      'i18n',
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
---
+++ frontend/src/boot/i18n.js
@@ -0,0 +1,14 @@
+import messages from 'src/i18n'
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+  locale: 'en-US',
+  messages
+})
+
+export default ({ app }) => {
+  // Set i18n instance on app
+  app.use(i18n)
+}
+
+export { i18n }
