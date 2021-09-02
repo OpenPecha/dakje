f867db69cec77507effd9ab5bedda19b5868369d
tenzin <ten13zin@gmail.com>
2021-09-02 23:53:05 +0530

    add:  tiptap editor

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>Index page</div>
+  <q-page padding class="column full-height">
+    <editor class="col" />
+  </q-page>
 </template>
 
 <script>
+import Editor from "components/Editor";
+
 export default {
   name: "Index",
+
+  components: {
+    Editor,
+  },
 };
 </script>
-
-<style lang="sass" scoped></style>
