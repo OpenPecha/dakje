a6a2a4a550f93c3b80f4f290d36fa3691a17ac81
tenzin <ten13zin@gmail.com>
2021-09-02 22:06:31 +0530

    add(frontend): logo and sidebar

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,147 @@
+<template>
+  <q-list class="text-uppercase">
+    <q-item header>
+      <q-item-section avatar>
+        <q-avatar class="full-width text-center">
+          <img
+            src="~assets/logo.svg"
+            class="cursor-pointer"
+            :title="$t('Home')"
+            @click="$emit('about')"
+          />
+        </q-avatar>
+      </q-item-section>
+    </q-item>
+    <q-separator />
+    <q-item v-ripple clickable :title="$t('Create a new file')" @click="$emit('newFile')">
+      <q-item-section avatar>
+        <q-icon name="add_circle" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("New...") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item
+      v-ripple
+      clickable
+      :title="$t('Open an existing file')"
+      @click="$emit('openFile')"
+    >
+      <q-item-section avatar>
+        <q-icon name="folder_open" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("Open") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item v-ripple clickable :title="$t('Save your work')" @click="$emit('saveFile')">
+      <q-item-section avatar>
+        <q-icon name="save" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("Save") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item
+      v-ripple
+      clickable
+      :title="$t('Save to destination')"
+      @click="$emit('saveFileAs')"
+    >
+      <q-item-section avatar>
+        <q-icon name="move_to_inbox" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("Save as") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-separator />
+    <q-item
+      v-ripple
+      clickable
+      :title="$t('Import from other formats')"
+      @click="$emit('importFile')"
+    >
+      <q-item-section avatar>
+        <q-icon name="cloud_upload" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("Import") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item
+      v-ripple
+      clickable
+      :title="$t('Export to another format')"
+      @click="$emit('exportFileAs')"
+    >
+      <q-item-section avatar>
+        <q-icon name="cloud_download" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("Export") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item v-ripple clickable :title="$t('Print your work')" @click="$emit('printFile')">
+      <q-item-section avatar>
+        <q-icon name="print" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("Print") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-separator />
+    <q-item
+      v-ripple
+      clickable
+      :title="$t('Adjust to your taste')"
+      @click="$emit('settings')"
+    >
+      <q-item-section avatar>
+        <q-icon name="settings" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("Settings") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-separator v-if="$q.platform.is.electron" />
+    <q-item
+      v-if="$q.platform.is.electron"
+      v-ripple
+      clickable
+      :title="$t('Close the application')"
+      @click="$emit('exit')"
+    >
+      <q-item-section avatar>
+        <q-icon name="exit_to_app" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ $t("Exit") }}</q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+export default {
+  name: "Siebar",
+  emits: [
+    "about",
+    "newFile",
+    "openFile",
+    "saveFile",
+    "saveFileAs",
+    "importFile",
+    "exportFileAs",
+    "printFile",
+    "settings",
+    "exit",
+  ],
+
+  computed: {
+    appInfo() {
+      return this.$store.getters["base/appInfo"];
+    },
+  },
+};
+</script>
--- frontend/src/i18n/en-US/index.js
+++ frontend/src/i18n/en-US/index.js
@@ -2,6 +2,15 @@
 // so you can safely delete all default props below
 
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
-}
+  "Toggle Menu": "Toggle Menu",
+  Home: "Home",
+  "Create a new file": "Create a new file",
+  "Open an existing file": "Open an existing file",
+  "Save your work": "Save your work",
+  "Save to destination": "Save to destination",
+  "Import from other formats": "Import from other formats",
+  "Export to another format": "Export to another format",
+  "Print your work": "Print your work",
+  "Adjust to your taste": "Adjust to your taste",
+  success: "Action was successful",
+};
