4d04968bdabc37013f033fcbc9a81f611345cc8f
tenzin <ten13zin@gmail.com>
2021-09-07 16:17:20 +0530

    add span to sentence schema

--- backend/app/app/schemas.py
+++ backend/app/app/schemas.py
@@ -1,4 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+
+
+class Span(BaseModel):
+    start: int
+    end: int
+
+    @validator("*")
+    def span_must_not_be_neg(cls, v):
+        if v < 0:
+            raise ValueError("span shouldn't be negative")
+        return v
+
+    @validator("end")
+    def end_must_not_be_less_than_start(cls, v, values, **kwargs):
+        if "start" in values and v < values["start"]:
+            raise ValueError("Span end must not be less than start")
+        return v
 
 
 class Text(BaseModel):
@@ -11,5 +28,7 @@ class Token(BaseModel):
 
 
 class Sentence(BaseModel):
+    content: str
     sentence_lenght: int
     verb_per_sentence: int
+    span: Span
